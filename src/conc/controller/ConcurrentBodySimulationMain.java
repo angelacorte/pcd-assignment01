1adbc01e03ea1c0b9ab45e793ccb7699e2a4863c
Angela Cortecchia <angela.cortecchia@hotmail.com>
2022-04-04 17:55:53 +0200

    main take args

--- src/conc/controller/ConcurrentBodySimulationMain.java
+++ src/conc/controller/ConcurrentBodySimulationMain.java
@@ -3,10 +3,15 @@ package conc.controller;
 public class ConcurrentBodySimulationMain {
 
     public static void main(String[] args) {
-        int nBodies = Integer.parseInt(args[0]);
-        int nSteps = Integer.parseInt(args[1]);
+        int nBodies = 2;
+        int nSteps = 2;
         //int nWorkers = Runtime.getRuntime().availableProcessors()+1;
-        int nWorkers = Integer.parseInt(args[2]);
+        int nWorkers = 2;
+        if(args.length > 0){
+            nBodies = Integer.parseInt(args[0]);
+            nSteps = Integer.parseInt(args[1]);
+            nWorkers = Integer.parseInt(args[2]);
+        }
         System.out.println("SIMULATION STARTED \n #BODIES = " + nBodies + "\n #STEPS = " + nSteps + "\n #WORKERS = " + nWorkers);
         Simulator sim = new Simulator(nBodies, nSteps, nWorkers);
         sim.execute();
