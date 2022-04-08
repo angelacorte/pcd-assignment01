121af3ac481de2ef0bc5e878308fa7f8006390a4
Angela Cortecchia <angela.cortecchia@hotmail.com>
2022-04-08 11:54:52 +0200

    refactor

--- src/conc/controller/ConcurrentBodySimulationMain.java
+++ src/conc/controller/ConcurrentBodySimulationMain.java
@@ -5,13 +5,7 @@ public class ConcurrentBodySimulationMain {
     public static void main(String[] args) {
         int nBodies = 2;
         int nSteps = 3;
-//        int nWorkers = Runtime.getRuntime().availableProcessors()+1;
         int nWorkers = 2;
-        if(args.length > 0){
-            nBodies = Integer.parseInt(args[0]);
-            nSteps = Integer.parseInt(args[1]);
-            nWorkers = Integer.parseInt(args[2]);
-        }
         Simulator sim = new Simulator(nBodies, nSteps, nWorkers);
         sim.execute();
     }
